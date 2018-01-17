305a13144d75bbee4953c13f9bea71e0cbecd133
Thomas Dwi Dinata <thomasdwidinata@gmail.com>
2018-01-17 22:39:11 +0700

    Fixed several Library bug

--- unnamed-library.cpp
+++ unnamed-library.cpp
@@ -47,7 +47,7 @@ using namespace std;
     {
         system("pause");
     }
-    void system_clear_screen()
+    void system_clear()
     {
         system("cls");
     }
@@ -57,93 +57,93 @@ using namespace std;
 #elif __APPLE__
     #include "TargetConditionals.h"
     #if TARGET_IPHONE_SIMULATOR
-        #error "Unsupported: iOS Simulator. This library is not indended to run on this OS."
+        #error "Unsupported: iOS Simulator. This library is not intended to run on this OS."
     #elif TARGET_OS_IPHONE
-        #error "Unsupported: iOS. This library is not indended to run on this OS."
+        #error "Unsupported: iOS. This library is not intended to run on this OS."
     #elif TARGET_OS_MAC
 	    #include <termios.h>
         void system_pause()
         {
             printf("Press any key to continue...");
-            getchar();
+            cin.get();
+            cin.ignore();
         }
-        void system_clear_screen()
+        void system_clear()
         {
             system("clear");
         }
         bool kbhit(void)
-		{
-		    struct termios oldt, newt;
-		    int ch;
-		    int oldf;
-		
-		    tcgetattr(STDIN_FILENO, &oldt);
-		    newt = oldt;
-		    newt.c_lflag &= ~(ICANON | ECHO);
-		    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-		    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-		    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-		
-		    ch = getchar();
-		
-		    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-		    fcntl(STDIN_FILENO, F_SETFL, oldf);
-		
-		    if(ch != EOF)
-		    {
-		        ungetc(ch, stdin);
-		        return true;
-		    }
-		
-		    return false;
-		}
+      	{
+      	    struct termios oldt, newt;
+      	    int ch;
+      	    int oldf;
+
+      	    tcgetattr(STDIN_FILENO, &oldt);
+      	    newt = oldt;
+      	    newt.c_lflag &= ~(ICANON | ECHO);
+      	    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+      	    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+      	    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+      	    ch = getchar();
+
+      	    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+      	    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+      	    if(ch != EOF)
+      	    {
+      	        ungetc(ch, stdin);
+      	        return true;
+      	    }
+
+      	    return false;
+      	}
     #else
-        #error "Unsupported: Unknown Apple Product. This library is not indended to run on this OS."
+        #error "Unsupported: Unknown Apple Product. This library is not intended to run on this OS."
     #endif
 #elif __linux__
-
 	#include <termios.h>
     void system_pause()
     {
         printf("Press enter to continue...");
         std::cin.get();
     }
-    void system_clear_screen()
+    void system_clear()
     {
         system("clear");
     }
     bool kbhit(void)
-	{
-	    struct termios oldt, newt;
-	    int ch;
-	    int oldf;
-	
-	    tcgetattr(STDIN_FILENO, &oldt);
-	    newt = oldt;
-	    newt.c_lflag &= ~(ICANON | ECHO);
-	    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-	
-	    ch = getchar();
-	
-	    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	    fcntl(STDIN_FILENO, F_SETFL, oldf);
-	
-	    if(ch != EOF)
-	    {
-	        ungetc(ch, stdin);
-	        return true;
-	    }
-	
-	    return false;
-	}
+  	{
+  	    struct termios oldt, newt;
+  	    int ch;
+  	    int oldf;
+
+  	    tcgetattr(STDIN_FILENO, &oldt);
+  	    newt = oldt;
+  	    newt.c_lflag &= ~(ICANON | ECHO);
+  	    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  	    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  	    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  	    ch = getchar();
+
+  	    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  	    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  	    if(ch != EOF)
+  	    {
+  	        ungetc(ch, stdin);
+  	        return true;
+  	    }
+
+  	    return false;
+  	}
 #elif __unix__ // all unices not caught above
-    #error "Unsupported: UNIX. This library is not indended to run on this OS."
+    #error "Unsupported: UNIX. This library is not intended to run on this OS."
 #elif defined(_POSIX_VERSION)
-    #error "Unsupported: POSIX Compatible System. This library is not indended to run on this OS."
+    #error "Unsupported: POSIX Compatible System. This library is not intended to run on this OS."
 #else
-    #error "Unsupported an unknown system. This library is not indended to run on this OS."
+    #error "Unsupported an unknown system. This library is not intended to run on this OS."
 #endif
 
 
@@ -160,8 +160,6 @@ int scanNumber() // Input sanitiser to make sure that user inputs only numbers, 
     }
     return choice;
 }
-
-
 
 void initRandomiser()
 {
